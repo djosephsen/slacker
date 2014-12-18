eb6effd80f2e8639d7cbc74581450434210b8b46
Dave Josephsen <dave@skeptech.org>
2014-12-18 10:08:19 -0600

    initial import

---
+++ listener.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+//	"github.com/gorilla/websocket"
+	"net/http"
+	"net/url"
+	"encoding/json"
+	"strings"
+	//"io/ioutil"
+)
+
+
+func StartRTM(token string) (error) {
+	resp, err := http.PostForm("https://slack.com/api/rtm.start", url.Values{"token": {token}})
+	if err != nil {
+		return fmt.Errorf("no dice with rtm.start: %v", err)
+	}
+	defer resp.Body.Close()
+
+	authResp:=new(AuthResponse)
+
+	//respout,_:=ioutil.ReadAll(resp.Body)
+	//fmt.Printf("%s",string(respout))
+
+	dec:=json.NewDecoder(resp.Body)
+	err=dec.Decode(authResp)
+	if err != nil {
+		return fmt.Errorf("Couldn't decode json. ERR: %v", err)
+	}
+	splitUrl := strings.Split(authResp.Url, "/")
+	splitUrl[2] = splitUrl[2] + ":443"
+	authResp.Url = strings.Join(splitUrl, "/")
+
+	fmt.Println(authResp)
+	return nil
+}
+
+func main(){
+	
+	err:=StartRTM(`xoxp-3057259082-3057259092-3247338018-9890ee`)
+	if err != nil{
+		fmt.Println(err)
+	}else{
+		fmt.Println(`ok`)
+	}
+}
