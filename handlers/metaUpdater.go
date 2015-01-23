27c125b3190b5eb16ed6fdf2567892070850119c
Dave Josephsen <dave@skeptech.org>
2015-01-23 08:16:24 -0600

    first generic handler

---
+++ handlers/metaUpdater.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	sl "github.com/djosephsen/slacker/slackerlib"
+)
+
+
+
+var MetaUpdater = sl.EventHandler{
+	Name: `Meta Updater`,
+	Usage:`keeps Sbot.Meta up to date using event traffic from Slackhq`,
+	Type: `*`,
+	Run:		metaUpdaterFunc,
+}
+
+func metaUpdaterFunc (hp *sl.HandlerPackage){
+	sl.Logger.Debug(`MetaUpdater:: got a: `, hp.Type)
+}
