54a1bb1f5801918104a504f33ba48ee8b93daf41
Dave Josephsen <dave@skeptech.org>
2015-02-05 23:42:03 -0600

    better syn

--- handlers/syn.go
+++ handlers/syn.go
@@ -2,14 +2,30 @@ package handlers
 
 import (
 	sl "github.com/djosephsen/slacker/slackerlib"
+	"time"
+	"math/rand"
 )
 
 var Syn = sl.MessageHandler{
 	Name: `Syn`,
-	Usage:`listen for '<botname> syn', respond with 'ack'`,
+	Usage:`listen for '<botname> (syn|ping)', respond accordingly`,
 	Method: `RESPOND`,
-	Pattern: `syn`,
+	Pattern: `(?i)(syn|ping)`,
 	Run:	func(e *sl.Event, match []string){
-		e.Reply(`ack`)	
+	 	now:=time.Now()
+	   rand.Seed(int64(now.Unix()))
+		replies := []string{
+			"yeah um.. pong?",
+			"WHAT?! jeeze.",
+			"what? oh, um SYNACKSYN? ENOSPEAKTCP.",
+			"RST (lulz)",
+			"64 bytes from go.away.your.annoying icmp_seq=0 ttl=42 time=42.596 ms",
+			"hmm?",
+			"ack. what?",
+			"pong. what?",
+			"yup. still here.",
+			"super busy just now.. Can I get back to you in like 5min?",
+		}
+		e.Reply(replies[rand.Intn(len(replies)-1)])	
 	},
 }
